dce5df672b8ab42a088f866e9f76eb891fc08e5a
AmishaSk <119852047+AmishaSk@users.noreply.github.com>
2022-12-08 16:50:37 +0530

    commit

--- src/main/java/com/employee/EmployeeProjectApplication.java
+++ src/main/java/com/employee/EmployeeProjectApplication.java
@@ -1,10 +1,7 @@
 package com.employee;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-//EpApplication
 public class EmployeeProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeProjectApplication.class, args);
